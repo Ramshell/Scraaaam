33f834557f1b88c7375671c4d16de0dd3690cfe3
EmanuelDubor <emanueldubora@hotmail.com>
2017-04-15 00:02:08 -0300

    Improved initDatabase script for simplicity

--- src/backend/initDatabase.js
+++ src/backend/initDatabase.js
@@ -2,36 +2,36 @@ import Contributor from "./models/Contributor"
 import Project from "./models/Project"
 import ProjectService from "./services/project.service"
 
+const contributorJoinProjectByIndex = (contributorIndex, projectIndex) => {
+    return ProjectService.addContributor(projects[projectIndex], contributors[contributorIndex])
+        .then(updated => {
+            projects[projectIndex] = updated.project
+            contributors[contributorIndex] = updated.contributor
+            return Promise.resolve('Success!')
+        })
+}
+
+let contributors = [
+    {name: "Nicolas"},
+    {name: "Emanuel"}
+]
+
+let projects = [
+    {title: "Scraaaam", description: "Scraaaam-ception!"},
+    {title: "FixJS", description: "Porque JS tambien necesita fix. Y monadas. Y amor, mucho amor."}
+]
+
 const initDatabase = () => {
     console.log('Initializing database...')
-
-    let contributors = [
-        {name: "Nicolas"},
-        {name: "Emanuel"}
-    ]
-    Contributor.create(contributors)
-        .then(saved => {
-            contributors = saved
-            let projects = [
-                {title: "Scraaaam", description: "Scraaaam-ception!"},
-                {title: "FixJS", description: "Porque JS tambien necesita fix. Y monadas. Y amor, mucho amor."}
-            ]
-            Project.create(projects)
-                .then(saved => {
-                    projects = saved
-                    ProjectService.addContributor(projects[0], contributors[0])
-                        .then(updated => {
-                            projects[0] = updated.project
-                            contributors[0] = updated.contributor
-                            ProjectService.addContributor(projects[0], contributors[1])
-                                .then(updated => {
-                                    projects[0] = updated.project
-                                    contributors[1] = updated.contributor
-                                    console.log('Done!')
-                                })
-                        })
-                })
-        })
+    Promise.all([
+        Contributor.create(contributors),
+        Project.create(projects)
+    ]).then(created => {
+        contributors = created[0]
+        projects = created[1]
+    }).then(_ => contributorJoinProjectByIndex(0, 0))
+        .then(_ => contributorJoinProjectByIndex(1, 0))
+        .then(_ => console.log('Done!'))
 }
 
 export default initDatabase
