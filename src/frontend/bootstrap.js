73de208b73503ea151ac72e61e72c1e5aa5b394f
EmanuelDubor <emanueldubora@hotmail.com>
2017-05-01 18:51:15 -0300

    Removed useless components and html templates.

--- src/frontend/bootstrap.js
+++ src/frontend/bootstrap.js
@@ -11,18 +11,15 @@ import {RouterModule}  from '@angular/router';
 import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {MomentModule} from 'angular2-moment';
 
+import TaskComponent from './app/components/task.component'
 import AppComponent from './app/components/app.component'
 import ProjectComponent from './app/components/project.component'
-import ProjectDetailComponent from './app/components/projectDetail.component'
 import ProjectListComponent from './app/components/projectList.component'
 import NewProjectComponent from './app/components/newProject.component'
 import NewTaskComponent from './app/components/newTask.component'
 import TaskListComponent from './app/components/taskList.component'
-import TaskComponent from './app/components/task.component'
-import TaskDetailComponent from './app/components/taskDetail.component'
 import CommentComponent from './app/components/comment.component'
 import CommentListComponent from './app/components/commentList.component'
-import MilestoneComponent from './app/components/milestone/milestone.component'
 import ActiveViewComponent from './app/components/activeView.component'
 import TaskFrameComponent from './app/components/taskFrame.component'
 
@@ -37,19 +34,16 @@ let router = RouterModule.forRoot([
     imports: [router, BrowserModule, FormsModule, HttpModule, InfiniteScrollModule, MomentModule],
     styleUrls: ['./style.css', './buttons.css'],
     declarations: [
+        TaskComponent,
         TaskFrameComponent,
         ActiveViewComponent,
-        MilestoneComponent,
         CommentListComponent,
         CommentComponent,
-        TaskDetailComponent,
-        TaskComponent,
         TaskListComponent,
         NewTaskComponent,
         NewProjectComponent,
         ProjectListComponent,
         ProjectComponent,
-        ProjectDetailComponent,
         AppComponent
     ],
     bootstrap: [AppComponent]
