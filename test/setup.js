56daa7aa993e8f0d65ac2085f306e4bb76f2579d
Nicolas Leutwyler <ramshellcinox@gmail.com>
2017-05-05 13:46:16 -0300

    add setup

---
+++ test/setup.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import mockgoose from "mockgoose"
+
+
+export function setupMocha() {
+  before("Mock mongoose", async() => {
+    await mockgoose(mongoose)
+    mongoose.connect('mongodb://localhost/projects')
+  })
+
+  after("Restore mongoose", done => {
+    mongoose.unmock(done);
+  })
+
+  afterEach("Reset mock mongo database", done => {
+    mockgoose.reset(done);
+  })
+}
