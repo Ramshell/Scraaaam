207ad762d25550211961e498d278c7e345ceb352
EmanuelDubor <emanueldubora@hotmail.com>
2017-05-27 10:12:27 -0300

    Tweaks to karma.conf.js and travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,39 @@
 module.exports = (config) => {
-  config.set({
-    basePath: 'test/frontend',
-    frameworks: [ 'mocha' ],
-    files: [ 'all_tests.js' ],
+    const conf = {
+        basePath: 'test/frontend',
+        frameworks: ['mocha'],
+        files: ['all_tests.js'],
 
-    preprocessors: {
-      "all_tests.js": ["webpack"]
-    },
-    webpack: require("./webpack.config"),
-    webpackMiddleware: {
-      stats: "errors-only"
-    },
-    node: {
-      fs: "empty"
-    },
+        preprocessors: {
+            "all_tests.js": ["webpack"]
+        },
+        webpack: require("./webpack.config"),
+        webpackMiddleware: {
+            stats: "errors-only"
+        },
+        node: {
+            fs: "empty"
+        },
 
-    reporters: [ 'mocha' ],
-    mochaReporter: { output: 'full' },
-    browserConsoleLogOptions: {
-      level: 'log', format: '%b %T: %m', terminal: true
-    },
-    port: 9876,
-    singleRun: true,
-  })
+        reporters: ['mocha'],
+        mochaReporter: {output: 'full'},
+        browserConsoleLogOptions: {
+            level: 'log', format: '%b %T: %m', terminal: true
+        },
+        port: 9876,
+        browsers: ['Chrome'],
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+        singleRun: true,
+    }
+
+    if (process.env.TRAVIS) {
+        cfg.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(conf)
 }
