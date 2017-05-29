1ade4159f9c7d5124e7b40a85cce9534b4fd0bd6
Nicolas Leutwyler <ramshellcinox@gmail.com>
2017-05-29 00:18:54 -0300

    Refactor gulp tasks

---
+++ gulp_scripts/lintTask.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp';
+import eslint from 'gulp-eslint'
+
+export function lintTask(src){
+  return () => {
+    gulp.src([`${src}/**/*.js`,'!node_modules/**'])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+    }
+}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,46 +1,21 @@
 'use strict';
 
 import gulp from 'gulp';
-import eslint from 'gulp-eslint'
-import gls from 'gulp-live-server';
-import babel from 'gulp-babel';
 import mocha from 'gulp-mocha';
+import { lintTask } from './gulp_scripts/lintTask'
+import { server, transpile } from './gulp_scripts/server'
+import { backend_tests } from './gulp_scripts/backendTests'
 
 const dirs = {
   src: 'src',
-  dest: 'build'
+  dest: 'dist',
+  test: 'test'
 };
 
-gulp.task('default', () => {
-  console.log('hello world')
-});
+gulp.task('lint', lintTask(dirs.src));
 
-gulp.task('lint', () => {
-    return gulp.src(['src/**/*.js','!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-});
+gulp.task('transpile', transpile(dirs.src, dirs.dest));
 
-gulp.task('transpile', () => {
-  gulp.src(['src/backend/**/*.js'])
-  .pipe(babel())
-  .pipe(gulp.dest('dist/backend/'));
-});
+gulp.task('server', ['transpile'], server(dirs.src, dirs.dest));
 
-gulp.task('server', ['transpile'], () => {
-  var server = gls.new('dist/backend/index.js');
-  server.start();
-  gulp.watch(['src/backend/**/*.js'], ['transpile']);
-  gulp.watch('dist/backend/index.js', () => server.start());
-});
-
-gulp.task('backend-test', () =>
-	gulp.src('test/backend/**/*.js', {read: false})
-		.pipe(mocha({
-      compilers: 'js:babel-core/register',
-      timeout: 120000,
-      globals: [ 'recursive'],
-      require: [ 'babel-polyfill'],
-    }))
-);
+gulp.task('backend-test', backend_tests(dirs.test));
